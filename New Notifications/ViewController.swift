2a8db5253ec02f580db3148fa9f554020d0ec3d8
Melissa Bain <apps@melissadbain.com>
2016-08-30 10:07:58 -0500

    Initial Commit

---
+++ New Notifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  New Notifications
+//
+//  Created by Melissa Bain on 8/30/16.
+//  Copyright © 2016 MB Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
